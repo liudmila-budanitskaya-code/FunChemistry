2c67b0bed8f657ccf45894fff01ed44521d90ac8
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2021-06-13 21:45:51 +0300

    working

--- app/src/main/java/site/budanitskaya/chemistryquiz/fine/ui/chipscreen/ChipsViewModel.kt
+++ app/src/main/java/site/budanitskaya/chemistryquiz/fine/ui/chipscreen/ChipsViewModel.kt
@@ -1,12 +1,10 @@
 package site.budanitskaya.chemistryquiz.fine.ui.chipscreen
 
-import android.text.SpannableString
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.runBlocking
 import site.budanitskaya.chemistryquiz.fine.datasource.ChipsDatasource
-import site.budanitskaya.chemistryquiz.fine.utils.StringFormatter
 import site.budanitskaya.chemistryquiz.fine.domain.mapReactionEntitiesToReactions
 import site.budanitskaya.chemistryquiz.fine.models.Reaction
 import java.lang.StringBuilder
@@ -17,12 +15,13 @@ class ChipsViewModel : ViewModel() {
     }
 
     private val allReactionsList = getAllReactionsList()
-    var reactionIndex = (allReactionsList.indices).random()
 
-    var reaction = allReactionsList[reactionIndex]
-
+    var reaction = getReaction(allReactionsList)
+    // и количество угаданных продуктов
     private var rawReagents =
         reaction.reagents
+
+
 
     private var rawProducts = reaction.products
 
@@ -40,14 +39,13 @@ class ChipsViewModel : ViewModel() {
 
     private var _reactionNumber = MutableLiveData(1)
     val reactionNumber: LiveData<Int>
-        get() = _reactionNumber
+        get() = _reactionNumber /**/
 
     fun setNewReaction() {
 
         _reactionNumber.value = _reactionNumber.value?.plus(1)
-        reactionIndex = (allReactionsList.indices).random()
 
-        reaction = allReactionsList[reactionIndex]
+        reaction = getReaction(allReactionsList)
 
         rawReagents = reaction.reagents
 
@@ -58,10 +56,8 @@ class ChipsViewModel : ViewModel() {
         rawCorrectProducts = mutableListOf(rawProducts[0], rawProducts[1])
         rawFirstReagent = rawReagents[0]
         rawSecondReagent = rawReagents[1]
-        val rawReagentList = mutableListOf<String>()
-        rawReagentList.add(rawFirstReagent)
-        rawReagentList.add(rawSecondReagent)
-        rawReagentsString = StringBuilder(rawReagentList.joinToString(" + ").plus(" = "))
+
+        superFunction()
 
 
     }
@@ -71,12 +67,6 @@ class ChipsViewModel : ViewModel() {
         rawReagentList.add(rawFirstReagent)
         rawReagentList.add(rawSecondReagent)
         rawReagentsString = StringBuilder(rawReagentList.joinToString(" + ").plus(" = "))
-
-
-        val spannableCorrectProducts = mutableListOf<SpannableString>()
-        rawCorrectProducts.forEach {
-            spannableCorrectProducts.add(StringFormatter.formatFormula(it))
-        }
     }
 
 
@@ -87,4 +77,9 @@ class ChipsViewModel : ViewModel() {
         }
         return allReactions
     }
+
+    fun getReaction(allReactions: List<Reaction>): Reaction{
+        return allReactions.shuffled()[0]
+    }
+
 }
